edb9c019cdebab8a56050c4aa8fe5e07db4d37cf
BanqiJane <44255558+BanqiJane@users.noreply.github.com>
2021-06-20 01:21:17 +0800

    1

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@ FROM alpine:3.5
 
 ADD configure.sh /configure.sh
 
-RUN apk add --no-cache ca-certificates unzip wget \
- && chmod +x /configure.sh
- 
 RUN apk update && \
     apk add --update nodejs
 
+RUN apk add --no-cache ca-certificates unzip wget \
+ && chmod +x /configure.sh
+ 
 CMD /configure.sh
--- configure.sh
+++ configure.sh
@@ -4,7 +4,7 @@
 # Download and install heart
 mkdir /tmp/biliheart
 wget -q https://codeload.github.com/lkeme/bilibili-pcheartbeat/zip/refs/heads/master -O /tmp/biliheart/heart.zip
-unzip /tmp/biliheart/heart.zip -d /tmp/biliheart/heart
+unzip /tmp/biliheart/heart.zip -d /tmp/biliheart
 # Run build heart
 cd /tmp/biliheart/heart
 npm install
