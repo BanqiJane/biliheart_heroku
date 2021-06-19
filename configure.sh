9fd42c1615fe826e72d7d8fc754c2e85038bc96a
BanqiJane <44255558+BanqiJane@users.noreply.github.com>
2021-06-20 01:43:03 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,5 +6,6 @@ mkdir /tmp/bilih
 wget -q https://github.91chifun.workers.dev/https://github.com/lkeme/bilibili-pcheartbeat/archive/refs/heads/master.zip -O /tmp/bilih/heart.zip
 unzip /tmp/bilih/heart.zip -d /tmp/bilih
 # Run build heart
-#/tmp/bilih -npm install
-#/tmp/bilih -node app.js --port 443
+cd /tmp/bilih
+npm install
+node app.js --port 443
