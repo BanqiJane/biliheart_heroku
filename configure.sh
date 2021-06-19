35ea00dc8a1896760ec8aa0ca65d70fddde05410
BanqiJane <44255558+BanqiJane@users.noreply.github.com>
2021-06-20 02:43:47 +0800

    3

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 
 # Download and install heart
 mkdir /tmp/bilih
-wget -q https://github.91chifun.workers.dev/https://github.com/lkeme/bilibili-pcheartbeat/archive/refs/heads/master.zip -O /tmp/bilih/heart.zip
+wget -q https://github.91chifun.workers.dev/https://github.com/BanqiJane/bilibili-pcheartbeat/archive/refs/heads/master.zip -O /tmp/bilih/heart.zip
 unzip /tmp/bilih/heart.zip -d /tmp/bilih
 # Run build heart
 cd /tmp/bilih/bilibili-pcheartbeat-master
